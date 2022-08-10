7a545f4226d8edde1520d6f8b81e6b461c599337
abdwFawzy <abdwfawzy985@gmail.com>
2022-08-10 15:46:19 +0200

    display functionality in dom

--- script.js
+++ script.js
@@ -4,67 +4,125 @@
    let choices = ['Rock', 'Paper', 'Scissors'];
 
     function computerPlay () {
-      let result = choices[Math.floor(Math.random() * choices.length)];
+      let result = choices[Math.floor(Math.random() * choices.length - 1)];
       return result;
     }
 
     // step 4 - function that plays 1 round R/P/S, and returns string
 
-    
 
-    function playRound (playerChoice, computerChoice) {
-
-    let playerSelection = prompt("Choose: Rock/Paper/Scissors",'');
-
-    let computerSelection = computerPlay();
-
-    if (playerSelection.toLowerCase() == "Rock".toLowerCase()) {
-        if (computerSelection === "Scissors") {
-            ++playerScore
-          return "You win! Rock beats Scissors.";
-        }else if(computerSelection === "Paper"){
-            ++compScore
-          return "You lose! Paper beats rock.";
-        }else {
-          return "Tie!"
-        }
-    }else if (playerSelection.toLowerCase() === "Paper".toLowerCase()) {
-      if (computerSelection === "Scissors") {
-            ++compScore
-          return "You lose! Scissors beats Paper!";
-        }else if (computerSelection === "Rock") {
-            ++playerScore
-          return "You win! Paper beats Rock!";
-        }else if(computerSelection === "Paper"){
-          return "Tie!";
-        }
-    }else if(playerSelection.toLowerCase() === "Scissors".toLowerCase()) {
-      if (computerSelection === "Rock") {
-            ++compScore
-          return "You lose! Rock beats Scissors!";
-        }else if (computerSelection === "Paper") {
-            ++playerScore
-          return "You win! Paper beats Rock!";
-        }else {
-          return "Tie!"
-        }
-    }
-  }
-
-   function game() {
-    for (let index = 0; index < 4; index++) {
-        console.log (playRound());
-    } 
-     if (playerScore > compScore) {
-         return "You Win the game!!!!";
-     }else if (playerScore < compScore) {
-         return "You Lost the game to a computer!";
-     }else {
-         return "It's a Draw!";
-     }
-    }
+       
 
     let playerScore = 0;
     let compScore = 0;
+  
 
+   
+   
+  const btns = document.querySelector('div');
+  let playerSelection = btns.addEventListener('click', event => {
+    playerSelection = event.target.textContent;
+    game()
+});
+  let p = document.createElement('p');
+  let body = document.querySelector('body');
+  body.appendChild(p);
+  function playRound (playerChoice, computerChoice) 
+  {
+      
+      
+    let computerSelection = computerPlay();
+
+    
+    if (playerSelection.toLowerCase() == "Rock".toLowerCase()) {
+        if (computerSelection === "Scissors") {
+          playerScore++
+
+         
+          p.textContent = "You win! Rock beats Scissors." ; 
+          
+        }else if(computerSelection === "Paper"){
+            compScore++
+  
+           
+            p.textContent = "You lose! Paper beats rock."; 
+          
+        }else {
+          p.textContent = "Tie!"
+          
+        }
+    }
+     else if (playerSelection.toLowerCase() === "Paper".toLowerCase()) {
+      if (computerSelection == "Scissors") {
+            compScore++
+  
+           
+          p.textContent = "You lose! Scissors beats Paper!";
+        }else if (computerSelection == "Rock") {
+            playerScore++
+  
+           
+           p.textContent = "You win! Paper beats Rock!";
+        }else if(computerSelection === "Paper"){
+          p.textContent = "Tie!";
+        }
+    }
+    else if(playerSelection.toLowerCase() === "Scissors".toLowerCase()) {
+      if (computerSelection === "Rock") {
+            compScore++
+  
+           
+          p.textContent = "You lose! Rock beats Scissors!";
+        }else if (computerSelection === "Paper") {
+            playerScore++
+  
+           
+          p.textContent = "You win! Scissors beats Paper!";
+        }else {
+          p.textContent = "Tie!"
+        }
+    }
+    else{
+      p.textContent = ''
+    }
+  }
+let count = 0;
+let doc_count = document.createElement('h1');
+body.appendChild(doc_count)
+   function game() {
+    count++;
+    doc_count.textContent = count;
+    if (count > 4){
+      if (playerScore > compScore) {
+        compScore = 0;
+          playerScore = 0;
+
+         
+          count = 0;
+          doc_count.textContent = count;
+           p.textContent ="You Win the game!!!!";
+          
+      }else if (playerScore < compScore) {
+        compScore = 0;
+          playerScore = 0;
+
+         
+          count = 0;
+          doc_count.textContent = count;
+          p.textContent ="You Lost the game to a computer!";
+      }else {
+        compScore = 0;
+          playerScore = 0;
+
+         
+          count = 0;
+          doc_count.textContent = count;
+          p.textContent ="It's a Draw!";
+      }
+    }
+    else{
+       console.log ( playRound()); 
+    }
+   
+  }
 
